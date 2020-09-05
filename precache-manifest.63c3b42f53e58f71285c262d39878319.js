d516a00c16cb0fdb1b3e8c8b54601a6ef7534eb8
Sunidhi Jain <33275429+SunidhiJain@users.noreply.github.com>
2020-09-05 22:09:06 +0530

    Add files via upload

---
+++ precache-manifest.63c3b42f53e58f71285c262d39878319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85310c539552308931427607c102bb40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1454468aa011f31e96f1",
+    "url": "/static/css/main.cad41ecf.chunk.css"
+  },
+  {
+    "revision": "2d5476d41dbc49cf9d4e",
+    "url": "/static/js/2.8b0642d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b0642d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1454468aa011f31e96f1",
+    "url": "/static/js/main.e4679e67.chunk.js"
+  },
+  {
+    "revision": "f41f514e95e21d8f263c",
+    "url": "/static/js/runtime-main.803c8f34.js"
+  },
+  {
+    "revision": "c7e77972e2987bf7fc0554a649889ac0",
+    "url": "/static/media/headphone.c7e77972.jpg"
+  }
+]);
